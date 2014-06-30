a6b73d6f67ae923e86a5a8f3ce2be7d0165bd845
Andrea Dal Ponte <info@andreadalponte.com>
2014-06-30 10:03:18 +0200

    created Manganese.reset_tenant! method

--- lib/manganese.rb
+++ lib/manganese.rb
@@ -6,17 +6,21 @@ require 'manganese/tenancy'
 
 module Manganese
   class << self
-    def current_tenant=(tenant)
+    def current_tenant=(database_name)
       self.default_db
-      Thread.current[:tenant] = tenant
+      Thread.current[:tenant] = database_name
     end
 
     def current_tenant
       Thread.current[:tenant] || self.default_db
     end
 
+    def reset_tenant!
+      Thread.current[:tenant] = nil
+    end
+
     def default_db
-      @default_db ||= Mongoid.default_session.options[:database]
+      @default_db ||= Mongoid.default_session.options[:database].to_s
     end
   end
 end
