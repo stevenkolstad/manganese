f8f43c7ebbd9d9d241847b6b8efa15aa18064ddd
Andrea Dal Ponte <info@andreadalponte.com>
2014-07-07 12:57:42 +0200

    Added Manganese.current_session and Manganese.with_tenant utils methods

--- lib/manganese.rb
+++ lib/manganese.rb
@@ -1,11 +1,17 @@
 require 'active_support/concern'
+
 require 'mongoid'
+
 require 'manganese/version'
 require 'manganese/tenant'
 require 'manganese/tenancy'
 
 module Manganese
   class << self
+    def default_db
+      @default_db ||= Mongoid.default_session.options[:database].to_s
+    end
+
     def current_tenant=(database_name)
       self.default_db
       Thread.current[:tenant] = database_name
@@ -19,8 +25,21 @@ module Manganese
       Thread.current[:tenant] = nil
     end
 
-    def default_db
-      @default_db ||= Mongoid.default_session.options[:database].to_s
+    # Retrive the current session
+    #
+    # Manganese.current_session do |session|
+    #   session[:sample_collection_name]
+    # end
+    def current_session(&block)
+      Mongoid.default_session().with(database: Manganese.current_tenant, &block)
+    end
+
+    def with_tenant(tenant, &block)
+      tmp_current_tenant = Manganese.current_tenant
+      Manganese.current_tenant = tenant
+      block.call
+    ensure
+      Manganese.current_tenant = tmp_current_tenant
     end
   end
 end
