5d26296fa9fe75423b3c27319529478722d9e288
Andrea Dal Ponte <info@andreadalponte.com>
2014-07-28 18:24:58 +0200

    some docs

--- lib/manganese.rb
+++ lib/manganese.rb
@@ -14,24 +14,46 @@ end
 
 module Manganese
   class << self
+    # Retrieve the main database
+    #
+    # @example Retrieve the main database
+    #   Manganese.default_db
     def default_db
       @default_db ||= Mongoid.default_session.options[:database].to_s
     end
 
+
+    # Set the current tenant database
+    #
+    # @example
+    #   Manganese.current_tenant = 'custom_tenant_name'
     def current_tenant=(database_name)
       self.default_db
       Thread.current[:tenant] = database_name
     end
 
+
+    # Get the current tenant
+    #
+    # @example
+    #   Manganese.current_tenant
+    #
+    # @return [ String ]
     def current_tenant
       Thread.current[:tenant] || self.default_db
     end
 
+    # Reset the current tenant database
+    #
+    # @example
+    #   Manganese.reset_tenant
+    #
+    # @return [ nil ]
     def reset_tenant!
       Thread.current[:tenant] = nil
     end
 
-    # Retrive the current session
+    # Retrieve the current session
     #
     # @example Retrieve a `sample_collection` in the current tenant
     #   Manganese.current_session do |session|
@@ -41,6 +63,13 @@ module Manganese
       Mongoid.default_session().with(database: Manganese.current_tenant, &block)
     end
 
+
+    # Helper method to switch temporary to another tenant
+    #
+    # @example
+    #   Manganese.with_tenant 'sample_tenant_name' do
+    #     # do something...
+    #   end
     def with_tenant(tenant, &block)
       tmp_current_tenant = Manganese.current_tenant
       Manganese.current_tenant = tenant
